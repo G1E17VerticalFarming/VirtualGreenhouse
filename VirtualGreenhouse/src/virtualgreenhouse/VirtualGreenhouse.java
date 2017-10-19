3162326ed12d60cd5a525a8e9061706998efcffe
MadsLunde <mads.lunde31@gmail.com>
2017-10-19 17:18:52 +0200

    Fixed null pointers

--- VirtualGreenhouse/src/virtualgreenhouse/VirtualGreenhouse.java
+++ VirtualGreenhouse/src/virtualgreenhouse/VirtualGreenhouse.java
@@ -10,12 +10,10 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketException;
 import java.util.LinkedList;
-import java.util.Queue;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import static virtualgreenhouse.IMessage.COMMAND;
 import static virtualgreenhouse.IMessage.DIRECTION;
-import static virtualgreenhouse.UDPConnection.PORT;
 
 /**
  *
@@ -27,52 +25,49 @@ public class VirtualGreenhouse implements IMessage, ICommands {
 
     /**
      * @param args the command line arguments
+     * @throws java.net.SocketException
      */
 
     public static void main(String[] args) throws SocketException, IOException {
         LinkedList<byte[]> queue = new LinkedList();
 
         GreenHouse gh = GreenHouse.getInstance();
-
-        Runnable UDPConnection = new Runnable() {
-            @Override
-            public void run() {
-                while (true) {
-                    try {
-                        DatagramSocket socket = new DatagramSocket(PORT);
-                        byte[] buffer = new byte[110];
-                        DatagramPacket packet = new DatagramPacket(buffer, buffer.length); // Create packet ready for receiving incoming UDP datagrams
-                        System.out.println("Server running on port " + PORT);
-                        socket.receive(packet);
-                        byte[] data = java.util.Arrays.copyOf(packet.getData(), packet.getLength());
-
-                        queue.addLast(data);
-
-                        data[COMMAND] = COMMAND + 64;
-                        data[DIRECTION] = 1;
-                        DatagramPacket replyPacket = new DatagramPacket(data, data.length);
-                        socket.send(replyPacket);
-                    } catch (SocketException ex) {
-                        Logger.getLogger(VirtualGreenhouse.class.getName()).log(Level.SEVERE, null, ex);
-                    } catch (IOException ex) {
-                        Logger.getLogger(VirtualGreenhouse.class.getName()).log(Level.SEVERE, null, ex);
-                    }
+        DatagramSocket socket = new DatagramSocket(PORT);
+        
+        Runnable UDPConnection = () -> {
+            while (true) {
+                try {
+                    
+                    byte[] buffer = new byte[110];
+                    DatagramPacket packet = new DatagramPacket(buffer, buffer.length); // Create packet ready for receiving incoming UDP datagrams
+                    System.out.println("Server running on port " + PORT);
+                    socket.receive(packet);
+                    byte[] data = java.util.Arrays.copyOf(packet.getData(), packet.getLength());
+                    
+                    queue.addLast(data);
+                    
+                    data[COMMAND] += 64;
+                    data[DIRECTION] = 1;
+                    
+                    DatagramPacket replyPacket = new DatagramPacket(data, data.length, packet.getAddress(), packet.getPort());
+                    socket.send(replyPacket);
+                } catch (SocketException ex) {
+                    Logger.getLogger(VirtualGreenhouse.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (IOException ex) {
+                    Logger.getLogger(VirtualGreenhouse.class.getName()).log(Level.SEVERE, null, ex);
                 }
             }
         };
 
-        Runnable decoderRunnable = new Runnable() {
-            @Override
-            public void run() {
-                ByteArrayDecoder bad = new ByteArrayDecoder(queue.poll());
-            }
+        Runnable decoderRunnable = () -> {
+            ByteArrayDecoder bad = new ByteArrayDecoder(queue.poll());
         };
 
         Thread connection = new Thread(UDPConnection);
         Thread execution = new Thread(decoderRunnable);
-        connection.setDaemon(true);
+        //connection.setDaemon(true);
         connection.start();
-        execution.setDaemon(true);
+        //execution.setDaemon(true);
         execution.start();
 
     }
