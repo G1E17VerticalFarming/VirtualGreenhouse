3a263363d24d89ced16e8d809fd53620d7dbed2a
kasper hoffmann <kasper-hoffmann@hotmail.com>
2017-11-08 11:07:39 +0100

    created popup for input of ip and port number to create a greenhouse

--- VirtualGreenhouse/src/virtualgreenhouse/GreenHouse.java
+++ VirtualGreenhouse/src/virtualgreenhouse/GreenHouse.java
@@ -6,15 +6,18 @@
 package virtualgreenhouse;
 
 import API.IGreenhouse;
+import java.net.InetAddress;
 import java.util.BitSet;
 import java.util.Date;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author chris
  */
 public class GreenHouse implements IGreenhouse {
-    
+
     private double temperature = 0;
     private double moisture = 0;
     private double waterLevel = 0;
@@ -22,239 +25,274 @@ public class GreenHouse implements IGreenhouse {
     private double plantHeight = 0.0;
     private int fanSpeed = 0;
     private int fertiliser = 0;
-    
+    private int port = 0;
+    private String ip = "";
+
     // light level
     private int blueLightLevel = 0;
     private int redLightLevel = 0;
-    
+
     private Date dateChecker;
-    
+
     private static GreenHouse instance = null;
-    
+
     protected GreenHouse() {
-        dateChecker = new Date();
-        this.GrowthRate();
+	dateChecker = new Date();
+	this.GrowthRate();
+
     }
-    
+
     public static GreenHouse getInstance() {
-        if(instance == null) {
-            instance = new GreenHouse();
-        }
-        return instance;
+	if (instance == null) {
+	    instance = new GreenHouse();
+	}
+	return instance;
     }
-    
+
     private void GrowthRate() {
-        // timeStart checks time at start of execution
-        long timeStart = dateChecker.getTime();
-        // timeSinceExe is used to only execute at specific times later than timeStart
-        long timeSinceExe = dateChecker.getTime();
-        
-        while(plantHeight < 30) {
-            if(timeSinceExe >= timeSinceExe && fertiliser < 1) {
-                timeSinceExe += 60000;
-                plantHeight++;
-            } else if(timeSinceExe >= timeSinceExe && fertiliser >= 50) {
-                timeSinceExe += 30000;
-                plantHeight++;
-                fertiliser--;
-            }
-        }
+	// timeStart checks time at start of execution
+	long timeStart = dateChecker.getTime();
+	// timeSinceExe is used to only execute at specific times later than timeStart
+	long timeSinceExe = dateChecker.getTime();
+
+	while (plantHeight < 30) {
+	    if (timeSinceExe >= timeSinceExe && fertiliser < 1) {
+		timeSinceExe += 60000;
+		plantHeight++;
+	    } else if (timeSinceExe >= timeSinceExe && fertiliser >= 50) {
+		timeSinceExe += 30000;
+		plantHeight++;
+		fertiliser--;
+	    }
+	}
     }
-    
+
     @Override
     public boolean SetTemperature(int kelvin) {
-        // timeTemp keeps track of time needed for execution
-        long timeTemp;
-        
-        boolean increaseTemp = true;
-        
-        // Check if kelvin is higher or lower than current temp
-        if(kelvin > temperature) {
-            timeTemp = ((int)kelvin - (int)temperature);
-        } else {
-            timeTemp = ((int)temperature - (int)kelvin);
-            increaseTemp = false;
-        }
-        
-        // timeStart checks time at start of execution
-        long timeStart = dateChecker.getTime();
-        // timeSinceExe is used to only execute at specific times later than timeStart
-        long timeSinceExe = dateChecker.getTime();
-        // finishTime is when the execution should be done
-        long finishTime = timeStart + (timeTemp * 1000);
-        
-        // While timeStart is lower than finishTime, execute and increase/decrease
-        while(timeStart <= finishTime) {
-            if(increaseTemp && dateChecker.getTime() >= timeSinceExe) {
-                timeSinceExe += 1000;
-                temperature++;
-            } else if(!increaseTemp && dateChecker.getTime() >= timeSinceExe){
-                timeSinceExe += 1000;
-                temperature--;
-            }
-        }
-        return true;
+	// timeTemp keeps track of time needed for execution
+	long timeTemp;
+
+	boolean increaseTemp = true;
+
+	// Check if kelvin is higher or lower than current temp
+	if (kelvin > temperature) {
+	    timeTemp = ((int) kelvin - (int) temperature);
+	} else {
+	    timeTemp = ((int) temperature - (int) kelvin);
+	    increaseTemp = false;
+	}
+
+	// timeStart checks time at start of execution
+	long timeStart = dateChecker.getTime();
+	// timeSinceExe is used to only execute at specific times later than timeStart
+	long timeSinceExe = dateChecker.getTime();
+	// finishTime is when the execution should be done
+	long finishTime = timeStart + (timeTemp * 1000);
+
+	// While timeStart is lower than finishTime, execute and increase/decrease
+	while (timeStart <= finishTime) {
+	    if (increaseTemp && dateChecker.getTime() >= timeSinceExe) {
+		timeSinceExe += 1000;
+		temperature++;
+	    } else if (!increaseTemp && dateChecker.getTime() >= timeSinceExe) {
+		timeSinceExe += 1000;
+		temperature--;
+	    }
+	}
+	return true;
     }
 
     @Override
     public boolean SetMoisture(int moist) {
-        // timeTemp keeps track of time needed for execution
-        long timeTemp;
-        
-        boolean increaseMoisture = true;
-        
-        // Check if moist is higher or lower than current moisture
-        if(moist > moisture) {
-            timeTemp = ((int)moist - (int)moisture);
-        } else {
-            timeTemp = ((int)moisture - (int)moist);
-            increaseMoisture = false;
-        }
-        
-        // timeStart checks time at start of execution
-        long timeStart = dateChecker.getTime();
-        // timeSinceExe is used to only execute at specific times later than timeStart
-        long timeSinceExe = dateChecker.getTime();
-        // finishTime is when the execution should be done
-        long finishTime = timeStart + (timeTemp * 1000);
-        
-        // While timeStart is lower than finishTime, execute and increase/decrease
-        while(timeStart <= finishTime) {
-            if(increaseMoisture && dateChecker.getTime() >= timeSinceExe) {
-                timeSinceExe += 1000;
-                moisture++;
-            } else if(!increaseMoisture && dateChecker.getTime() >= timeSinceExe){
-                timeSinceExe += 1000;
-                moisture--;
-            }
-        }
-        return true;
+	// timeTemp keeps track of time needed for execution
+	long timeTemp;
+
+	boolean increaseMoisture = true;
+
+	// Check if moist is higher or lower than current moisture
+	if (moist > moisture) {
+	    timeTemp = ((int) moist - (int) moisture);
+	} else {
+	    timeTemp = ((int) moisture - (int) moist);
+	    increaseMoisture = false;
+	}
+
+	// timeStart checks time at start of execution
+	long timeStart = dateChecker.getTime();
+	// timeSinceExe is used to only execute at specific times later than timeStart
+	long timeSinceExe = dateChecker.getTime();
+	// finishTime is when the execution should be done
+	long finishTime = timeStart + (timeTemp * 1000);
+
+	// While timeStart is lower than finishTime, execute and increase/decrease
+	while (timeStart <= finishTime) {
+	    if (increaseMoisture && dateChecker.getTime() >= timeSinceExe) {
+		timeSinceExe += 1000;
+		moisture++;
+	    } else if (!increaseMoisture && dateChecker.getTime() >= timeSinceExe) {
+		timeSinceExe += 1000;
+		moisture--;
+	    }
+	}
+	return true;
     }
 
     @Override
     public boolean SetRedLight(int level) {
-        if (level < 100 || level > 0) {
-            return false;
-        } else {
-            this.redLightLevel = level;
-            return true;
-        }
+	if (level < 100 || level > 0) {
+	    return false;
+	} else {
+	    this.redLightLevel = level;
+	    return true;
+	}
     }
 
     @Override
     public boolean SetBlueLight(int level) {
-        if (level < 100 || level > 0) {
-            return false;
-        } else {
-            this.blueLightLevel = level;
-            return true;
-        }
+	if (level < 100 || level > 0) {
+	    return false;
+	} else {
+	    this.blueLightLevel = level;
+	    return true;
+	}
     }
 
     @Override
     public boolean AddWater(int sec) {
-        // timeStart checks time at start of execution
-        long timeStart = dateChecker.getTime();
-        // timeSinceExe is used to only execute at specific times later than timeStart
-        long timeSinceExe = dateChecker.getTime();
-        // finishTime is when the execution should be done
-        long finishTime = timeStart + (sec * 1000);
-        
-        while(timeStart <= finishTime) {
-            if(dateChecker.getTime() >= timeSinceExe) {
-                timeSinceExe += 1000;
-                waterLevel++;
-            }
-        }
-        return true;
+	// timeStart checks time at start of execution
+	long timeStart = dateChecker.getTime();
+	// timeSinceExe is used to only execute at specific times later than timeStart
+	long timeSinceExe = dateChecker.getTime();
+	// finishTime is when the execution should be done
+	long finishTime = timeStart + (sec * 1000);
+
+	while (timeStart <= finishTime) {
+	    if (dateChecker.getTime() >= timeSinceExe) {
+		timeSinceExe += 1000;
+		waterLevel++;
+	    }
+	}
+	return true;
     }
 
     @Override
     public boolean AddFertiliser(int sec) {
-        // timeStart checks time at start of execution
-        long timeStart = dateChecker.getTime();
-        // timeSinceExe is used to only execute at specific times later than timeStart
-        long timeSinceExe = dateChecker.getTime();
-        // finishTime is when the execution should be done
-        long finishTime = timeStart + (sec * 1000);
-        
-        while(timeStart <= finishTime) {
-            if(dateChecker.getTime() >= timeSinceExe) {
-                timeSinceExe += 1000;
-                fertiliser++;
-            }
-        }
-        return true;
+	// timeStart checks time at start of execution
+	long timeStart = dateChecker.getTime();
+	// timeSinceExe is used to only execute at specific times later than timeStart
+	long timeSinceExe = dateChecker.getTime();
+	// finishTime is when the execution should be done
+	long finishTime = timeStart + (sec * 1000);
+
+	while (timeStart <= finishTime) {
+	    if (dateChecker.getTime() >= timeSinceExe) {
+		timeSinceExe += 1000;
+		fertiliser++;
+	    }
+	}
+	return true;
     }
 
     @Override
     public boolean AddCO2(int sec) {
-        // timeStart checks time at start of execution
-        long timeStart = dateChecker.getTime();
-        // timeSinceExe is used to only execute at specific times later than timeStart
-        long timeSinceExe = dateChecker.getTime();
-        // finishTime is when the execution should be done
-        long finishTime = timeStart + (sec * 1000);
-        
-        while(timeStart <= finishTime) {
-            if(dateChecker.getTime() >= timeSinceExe) {
-                timeSinceExe += 1000;
-                if(co2Level < 100) { 
-                    co2Level++;
-                }
-            }
-        }
-        return true;
+	// timeStart checks time at start of execution
+	long timeStart = dateChecker.getTime();
+	// timeSinceExe is used to only execute at specific times later than timeStart
+	long timeSinceExe = dateChecker.getTime();
+	// finishTime is when the execution should be done
+	long finishTime = timeStart + (sec * 1000);
+
+	while (timeStart <= finishTime) {
+	    if (dateChecker.getTime() >= timeSinceExe) {
+		timeSinceExe += 1000;
+		if (co2Level < 100) {
+		    co2Level++;
+		}
+	    }
+	}
+	return true;
     }
 
     @Override
     public double ReadTemp1() {
-        return temperature;
+	return temperature;
     }
 
     @Override
     public double ReadTemp2() {
-        return temperature;
+	return temperature;
     }
 
     @Override
     public double ReadMoist() {
-        return moisture;
+	return moisture;
     }
 
     @Override
     public double ReadWaterLevel() {
-        return waterLevel;
+	return waterLevel;
     }
 
     @Override
     public double ReadPlantHeight() {
-        return plantHeight;
+	return plantHeight;
     }
 
     @Override
     public BitSet ReadErrors() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public boolean ResetError(int errorNum) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public byte[] GetStatus() {
-        return new byte[1];
+	return new byte[1];
     }
 
     @Override
     public boolean SetFanSpeed(int speed) {
-        if (speed < 100 || speed > 0) {
-            return false;
-        } else {
-            this.fanSpeed = speed;
-            return true;
-        }
+	if (speed < 100 || speed > 0) {
+	    return false;
+	} else {
+	    this.fanSpeed = speed;
+	    return true;
+	}
+    }
+
+    public void askForIP() {
+
+	JFrame frame = new JFrame("InputDialog Example #1");
+	String ip = JOptionPane.showInputDialog(frame, "Which Ip do you wish to connect to?");
+
+	
+	if(JOptionPane.INPUT_VALUE_PROPERTY != ip  ){
+	    System.out.println("Something whent wrong, please try again, and this time input a valid IP");
+	}
+	
+	ip = JOptionPane.INPUT_VALUE_PROPERTY;
+	System.out.printf("The wished ip is '%s'.\n", ip);
+	System.exit(0);
+	
+	
+
     }
     
-}
+    public void askForPort(){
+	JFrame frame = new JFrame("InputDialog Example #1");
+	String ip = JOptionPane.showInputDialog(frame, "Which port do you wish to connect to?");
+
+	
+	if((JOptionPane.INPUT_VALUE_PROPERTY != port){
+	    System.out.println("Something whent wrong, please try again, and this time input a valid port");
+	}
+
+	port = JOptionPane.INPUT_VALUE_PROPERTY;
+	System.out.printf("The wished port is '%s'.\n", port);
+	System.exit(0);
+    }
+
+}	
--- VirtualGreenhouse/src/virtualgreenhouse/VirtualGreenhouse.java
+++ VirtualGreenhouse/src/virtualgreenhouse/VirtualGreenhouse.java
@@ -6,12 +6,18 @@
 package virtualgreenhouse;
 
 import java.io.IOException;
+import static java.net.Authenticator.RequestorType.SERVER;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.SocketException;
+import java.sql.Connection;
 import java.util.LinkedList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.sound.sampled.Port;
+import javax.swing.JOptionPane;
+import sun.security.x509.IPAddressName;
 import static virtualgreenhouse.IMessage.COMMAND;
 import static virtualgreenhouse.IMessage.DIRECTION;
 
@@ -21,64 +27,79 @@ import static virtualgreenhouse.IMessage.DIRECTION;
  */
 public class VirtualGreenhouse implements IMessage, ICommands {
 
+   
     public static final int PORT = 1025;
+    public GreenHouse green = new GreenHouse();
+    String fixedServerName;
+    private Connection connection;
 
     /**
      * @param args the command line arguments
      * @throws java.net.SocketException
      */
+    public GreenHouse createGreenhouse(String IPAddressName, int PORT, String name) {
+
+	GreenHouse greenhouse = new GreenHouse();
+	return greenhouse;
+    }
 
     public static void main(String[] args) throws SocketException, IOException {
-        LinkedList<byte[]> queue = new LinkedList();
+	VirtualGreenhouse vgh = new VirtualGreenhouse();
+	LinkedList<byte[]> queue = new LinkedList();
+	vgh.green.activate();
+	
 
-        GreenHouse gh = GreenHouse.getInstance();
-        DatagramSocket socket = new DatagramSocket(PORT);
-        
-        Runnable UDPConnection = () -> {
-            while (true) {
-                try {
-                    
-                    byte[] buffer = new byte[110];
-                    DatagramPacket packet = new DatagramPacket(buffer, buffer.length); // Create packet ready for receiving incoming UDP datagrams
-                    System.out.println("Server running on port " + PORT);
-                    socket.receive(packet);
-                    byte[] data = java.util.Arrays.copyOf(packet.getData(), packet.getLength());
-                    
-                    queue.addLast(data);
-                    
-                    for (int i = 9; i <= 15; i++) {
-                        if(data[COMMAND] == i) {
-                            ByteArrayDecoder bad = new ByteArrayDecoder(data);
-                            data[DATA_START] += bad.decoder();
-                        }
-                    }
-                    
-                    data[COMMAND] += 64;
-                    data[DIRECTION] = 1;
-                    
-                    DatagramPacket replyPacket = new DatagramPacket(data, data.length, packet.getAddress(), packet.getPort());
-                    socket.send(replyPacket);
-                } catch (SocketException ex) {
-                    Logger.getLogger(VirtualGreenhouse.class.getName()).log(Level.SEVERE, null, ex);
-                } catch (IOException ex) {
-                    Logger.getLogger(VirtualGreenhouse.class.getName()).log(Level.SEVERE, null, ex);
-                }
-            }
-        };
+	GreenHouse gh = GreenHouse.getInstance();
+	DatagramSocket socket = new DatagramSocket(PORT);
+	
 
-        Runnable decoderRunnable = () -> {
-            ByteArrayDecoder bad = new ByteArrayDecoder(queue.poll());
-            bad.decoder();
-        };
+	Runnable UDPConnection = () -> {
+	    while (true) {
+		try {
 
-        Thread connection = new Thread(UDPConnection);
-        Thread execution = new Thread(decoderRunnable);
-        //connection.setDaemon(true);
-        connection.start();
-        //execution.setDaemon(true);
-        if(queue.size()!=0) {
-        execution.start();
-        }
+		    byte[] buffer = new byte[110];
+		    DatagramPacket packet = new DatagramPacket(buffer, buffer.length); // Create packet ready for receiving incoming UDP datagrams
+		    System.out.println("Server running on port " + PORT);
+		    socket.receive(packet);
+		    byte[] data = java.util.Arrays.copyOf(packet.getData(), packet.getLength());
+		    
+
+		    queue.addLast(data);
+
+		    for (int i = 9; i <= 15; i++) {
+			if (data[COMMAND] == i) {
+			    ByteArrayDecoder bad = new ByteArrayDecoder(data);
+			    data[DATA_START] += bad.decoder();
+			}
+		    }
+
+		    data[COMMAND] += 64;
+		    data[DIRECTION] = 1;
+
+		    DatagramPacket replyPacket = new DatagramPacket(data, data.length, packet.getAddress(), packet.getPort());
+		    socket.send(replyPacket);
+		} catch (SocketException ex) {
+		    Logger.getLogger(VirtualGreenhouse.class.getName()).log(Level.SEVERE, null, ex);
+		} catch (IOException ex) {
+		    Logger.getLogger(VirtualGreenhouse.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	    }
+	};
+
+	Runnable decoderRunnable = () -> {
+	    ByteArrayDecoder bad = new ByteArrayDecoder(queue.poll());
+	    bad.decoder();
+	};
+
+	Thread connection = new Thread(UDPConnection);
+	Thread execution = new Thread(decoderRunnable);
+	//connection.setDaemon(true);
+	connection.start();
+	//execution.setDaemon(true);
+	if (queue.size() != 0) {
+	    execution.start();
+	}
 
     }
+
 }
